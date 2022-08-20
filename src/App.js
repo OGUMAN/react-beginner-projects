67c9117c65871ce7424ea07432daa24a5c8cb858
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-08-20 12:47:22 +0300

    Added UseState to make score working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [score, setScore] = useState(0);
+
+  const onClickPlus = () => {
+    setScore (score + 1);
+  }
+
+  const onClickMinus = () => {
+    setScore (score - 1);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{score}</h1>
+        <button onClick={onClickMinus} className="minus">- Минус</button>
+        <button onClick={onClickPlus} className="plus">Плюс +</button>
       </div>
     </div>
   );

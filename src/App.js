881dda46063ea646cdb46d54f2ab328012b1038d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-08-21 16:55:21 +0300

    Currencies are comparing and changing

--- src/App.js
+++ src/App.js
@@ -3,10 +3,56 @@ import { Block } from './Block';
 import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState('UAH');
+  const [toCurrency, setToCurrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(1);
+  
+  // const [rates, setRates] = React.useState({});
+  const ratesRef = React.useRef({})
+
+  React.useEffect(() => {
+    fetch('https://cdn.cur.su/api/latest.json')
+    .then((res) => res.json())
+    .then((json) => {
+      ratesRef.current = json.rates;
+      onChangeToPrice(1)
+    }).catch(err => {
+      console.warn(err);
+      alert('Failed to get information');
+    })
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
+    setFromPrice(value);
+    setToPrice(result.toFixed(3));
+  }
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setToPrice(value);
+    setFromPrice(result.toFixed(3));
+  }
+
+  const onChangeFromCurrency = (cur) => {
+    setFromCurrency(cur);
+    onChangeFromPrice(fromPrice);
+  }
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  React.useEffect(() => {
+    onChangeToPrice(fromPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={onChangeFromCurrency} onChangeValue={onChangeFromPrice} />
+      <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice} />
     </div>
   );
 }

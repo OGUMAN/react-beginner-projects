8c74cdc9d6fcbd4327c719027976410d46740f54
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-08-21 15:24:17 +0300

    Users are uploaded and can be invited

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,9 +4,9 @@ export const Success = ({ count }) => {
   return (
     <div class="success-block">
       <img src="/assets/success.svg" alt="Success" />
-      <h3>Successfully!</h3>
-      <p>All {count} users have been sent an invitation.</p>
-      <button onClick={() => {window.location.reload()}} className="send-invite-btn">Back</button>
+      <h3>Успешно!</h3>
+      <p>Всем {count} пользователям отправлено приглашение.</p>
+      <button onClick={() => {window.location.reload()}} className="send-invite-btn">Назад</button>
     </div>
   );
 };
--- src/components/Users/index.jsx
+++ src/components/Users/index.jsx
@@ -9,7 +9,7 @@ export const Users = ({ items, isLoading, searchValue, onCnangeSearchValue, invi
         <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
           <path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" />
         </svg>
-        <input value={searchValue} onChange={onCnangeSearchValue} type="text" placeholder="Find user..." />
+        <input value={searchValue} onChange={onCnangeSearchValue} type="text" placeholder="Найти пользователя..." />
       </div>
       {isLoading ? (
         <div className="skeleton-list">
@@ -36,7 +36,7 @@ export const Users = ({ items, isLoading, searchValue, onCnangeSearchValue, invi
         </ul>
         )}
         {
-          invites.length > 0 && <button onClick={onClickSendInvites} className="send-invite-btn">Send invitation</button>
+          invites.length > 0 && <button onClick={onClickSendInvites} className="send-invite-btn">Отправить приглашение</button>
         }
       </>
   );
